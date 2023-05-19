9d351e420a9134c8d1e50158f72281cfedd94962
RaviRathii <55147838+RaviRathii@users.noreply.github.com>
2023-05-19 09:58:25 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose');
 //const dotenv=require('dotenv')
 //dotenv.config({path: 'config.env'})
 //mongoose.connect("mongodb+srv://Ravi:COtJieb3AxMIo9AD@cluster0.qnb48ii.mongodb.net/")
-mongoose.connect('mongodb+srv://Ravi:COtJieb3AxMIo9AD@cluster0.qnb48ii.mongodb.net/',  { 
+mongoose.connect('mongodb+srv://ravirathi:m7kJHvzDxgiVt9OX@cluster0.5u8uegm.mongodb.net/csvUploader?retryWrites=true&w=majority',  { 
        useNewUrlParser: true,
        //useFindAndModify: false,
        useUnifiedTopology: true
